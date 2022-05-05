c29f27aab3e349664a901cba059773fcd71a09ac
KacperLidzbarski <kacper.lidzbarski@contractors.roche.com>
2022-05-05 19:05:21 +0200

    initial

---
+++ src/test/java/BlankTemplate.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Configuration;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.$;
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+
+public class BlankTemplate {
+
+
+    public static void main(String[] args) {
+        Configuration.browserSize = "1366x768"; //set browser size
+        open("insert page address here");
+
+//------------start test here---------------------------------------
+
+
+
+
+
+
+
+//------------end test here-----------------------------------------
+    }
+}
---
+++ src/test/java/TestExample.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class TestExample {
+
+
+    public static void main(String[] args) {
+        Configuration.browserSize = "1920x1080";
+        open("https://craftware.pl/"); //Otwieramy stronę Craftware
+
+        $(By.id("cookie_action_close_header")).click(); //zamykamy cookies
+        $(By.xpath("//span[text()='Staffing']")).hover(); //najeżdżamy kursorem na "Staffing"
+        $(By.xpath("//span[text()='Tester automatyzujący']")).click(); //klikamy na "tester autmatyzujący"
+        $(By.xpath("//div[@class='pageHeader__main']//h1")).shouldBe(Condition.text("Tester automatyzujący")); //sprawdzamy czy tytuł strony się zgadza
+
+
+
+
+    }
+}
