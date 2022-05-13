f371fc77dc5c647b297e5102d9f4a4eaf8184ecf
KacperLidzbarski <kacper.lidzbarski@contractors.roche.com>
2022-05-13 11:46:41 +0200

    small changes

--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -7,13 +7,13 @@ import static com.codeborne.selenide.Selenide.*;
 public class TestExample {
 
     public static void main(String[] args) {
-        Configuration.browserSize = "1920x1080";
+
         open("https://craftware.pl/"); //Otwieramy stronę Craftware
 
         $(By.id("cookie_action_close_header")).click(); //zamykamy cookies
-        $(By.xpath("//span[text()='Staffing']")).hover(); //najeżdżamy kursorem na "Staffing"
+        $(By.xpath("//span[text()='Staffing']")).click(); //klikamy na "Staffing"
         $(By.xpath("//span[text()='Tester automatyzujący']")).click(); //klikamy na "tester autmatyzujący"
-        $(By.xpath("//div[@class='pageHeader__main']//h1")).shouldBe(Condition.text("Tester automatyzujący")); //sprawdzamy czy tytuł na stronie zgadza (tekst)
+        $(By.xpath("//div[@class='pageHeader__main']//h1")).shouldHave(Condition.text("Tester automatyzujący")); //sprawdzamy czy tytuł na stronie zgadza (tekst)
 
 
 
